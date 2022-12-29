df4d9f8474fc494e12b5af93acb2a209fcc8c832
dipanjyoti <dipanjyotipaul@gmail.com>
2022-12-30 04:03:03 +0530

    updated with add

---
+++ add.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+
+def add(a,b):
+    return np.add(a, b)
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 from mul import mul
+from add import add
 
 
 
@@ -11,5 +12,5 @@ B=np.array([-1,-2,-3,-4])
 
 
 print (mul(A,B))
-
+print (add(A,B))
 
