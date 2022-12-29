fd2d38672a8e6103ccf7538f619d75cf301a9fe6
dipanjyoti <dipanjyotipaul@gmail.com>
2022-12-30 02:19:14 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+from mul import mul
+
+
+
+
+A=np.array([1,2,3,4])
+B=np.array([-1,-2,-3,-4])
+
+
+print (mul(A,B))
+
+
---
+++ mul.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+
+def mul(a,b):
+    return np.multiply(a, b)
+
+
